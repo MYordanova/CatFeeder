469e38a9e7ac75c27973aa02fd2808b60df008fd
thorahornburg <thora.hornburg@student.htw-berlin.de>
2018-01-16 02:12:40 +0100

    broken code fixed

--- js/main.js
+++ js/main.js
@@ -8,9 +8,6 @@ $(document).ready (function() {
   $('.navientry').on('click', function() {
     $('.navientry').removeClass('activated');
     $(this).addClass('activated');
-<<<<<<< HEAD
-=======
-
   });
 
 // Cat Profile Buttons
@@ -20,8 +17,7 @@ $(document).ready (function() {
   $('#buttonAddHabit').click(function(){
       $('.confirmHabit').show();
       $(this).hide();
->>>>>>> 22dc4ebf21ca707900b9fe251e1e783704d32f93
   });
 
-  
+
 });
