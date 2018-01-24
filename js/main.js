4a3aba963ffe53949b272148d36809c4e3a15e3f
thorahornburg <thora.hornburg@student.htw-berlin.de>
2018-01-25 00:49:12 +0100

    autofeed duration
    - tag/monat/jahr auswählbar
    - checkbox verknüpft

--- js/main.js
+++ js/main.js
@@ -196,18 +196,376 @@ $(document).ready(function() {
       $('#autofeedToggle').removeClass('on');
       $('#autofeedToggle').addClass('off');
       $('#autofeedToggleCircle').css('transform', 'translateX(0px)');
-
       $('.autofeedPosition').text('OFF');
+      $('.autofeedOption').text('');
     }
     // wenns aus ist : an machen
     else if($('#autofeedToggle').hasClass('off')) {
       $('#autofeedToggle').removeClass('off');
       $('#autofeedToggle').addClass('on');
       $('#autofeedToggleCircle').css('transform', 'translateX(20px)');
-
       $('.autofeedPosition').text('ON');
     }
   });
+
+  //autofeed buttons
+  $('#closeButtonAutofeed').hide();
+  $('#confirmButtonAutofeed').hide();
+
+  $('#editButtonAutofeed').click(function(){
+    $(this).addClass('activated');
+    setTimeout(function() {
+      $('#editButtonAutofeed').hide();
+      $('#closeButtonAutofeed').show();
+      $('#confirmButtonAutofeed').show();
+      $('.datePickerContent').removeClass('border');
+      $('.datePicker').addClass('border');
+    }, 100);
+  });
+
+  $('#closeButtonAutofeed').click(function(){
+    $(this).addClass('activated');
+    $('#editButtonAutofeed').removeClass('activated');
+    setTimeout(function() {
+      $('#closeButtonAutofeed').removeClass('activated');
+      $('#closeButtonAutofeed').hide();
+      $('#editButtonAutofeed').show();
+      $('#confirmButtonAutofeed').hide();
+      $('.datePicker').removeClass('border');
+      $('.datePickerContent').addClass('border');
+    }, 100);
+  });
+
+  $('#confirmButtonAutofeed').click(function(){
+    $(this).addClass('activated');
+    $('#editButtonAutofeed').removeClass('activated');
+    setTimeout(function() {
+      $('#confirmButtonAutofeed').removeClass('activated');
+      $('#confirmButtonAutofeed').hide();
+      $('#editButtonAutofeed').show();
+      $('#closeButtonAutofeed').hide();
+      $('.datePicker').removeClass('border');
+      $('.datePickerContent').addClass('border');
+    }, 100);
+  });
+
+
+  //autofeed date
+  var currdate = new Date();
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  var i = currdate.getMonth();
+  var DEFAULTS = {
+    currday: currdate.getDate(),
+    currmonth: month[i],
+    curryear: currdate.getFullYear(),
+    nextday: (currdate.getDate() + 1)
+  };
+  var newBeginDay = DEFAULTS.currday;
+  var newEndDay = DEFAULTS.nextday;
+  var newBeginMonth = DEFAULTS.currmonth;
+  var newEndMonth = DEFAULTS.currmonth;
+  var newBeginYear = DEFAULTS.curryear;
+  var newEndYear = DEFAULTS.curryear;
+
+  // DEFAULT werte ausgeben
+  $('#datePickerBeginDay').append(DEFAULTS.currday);
+  $('#datePickerEndDay').append(DEFAULTS.nextday);
+  $('#datePickerBeginMonth, #datePickerEndMonth').append(DEFAULTS.currmonth);
+  $('#datePickerBeginYear, #datePickerEndYear').append(DEFAULTS.curryear);
+
+  // day
+  $('#datePickerBeginDay').on('swipeup', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
+        if(newBeginDay < 31) {
+          newBeginDay = newBeginDay + 1;
+          $(this).append(newBeginDay);
+        } else {
+          newBeginDay = newBeginDay - 30;
+          $(this).append(newBeginDay);
+        }
+      } else
+      if(i == 3 || i == 5 || i == 8 || i == 10) {
+        if(newBeginDay < 30) {
+          newBeginDay = newBeginDay + 1;
+          $(this).append(newBeginDay);
+        } else {
+          newBeginDay = newBeginDay - 29;
+          $(this).append(newBeginDay);
+        }
+      } else
+      if(i == 1) {
+        if(newBeginDay < 28) {
+          newBeginDay = newBeginDay + 1;
+          $(this).append(newBeginDay);
+        } else {
+          newBeginDay = newBeginDay - 27;
+          $(this).append(newBeginDay);
+        }
+      }
+    }
+  });
+  $('#datePickerBeginDay').on('swipedown', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
+        if(newBeginDay > 1) {
+          newBeginDay = newBeginDay - 1;
+          $(this).append(newBeginDay);
+        } else {
+          newBeginDay = newBeginDay + 30;
+          $(this).append(newBeginDay);
+        }
+      } else
+      if(i == 3 || i == 5 || i == 8 || i == 10) {
+        if(newBeginDay > 1) {
+          newBeginDay = newBeginDay - 1;
+          $(this).append(newBeginDay);
+        } else {
+          newBeginDay = newBeginDay + 29;
+          $(this).append(newBeginDay);
+        }
+      } else
+      if(i == 1) {
+        if(newBeginDay > 1) {
+          newBeginDay = newBeginDay - 1;
+          $(this).append(newBeginDay);
+        } else {
+          newBeginDay = newBeginDay + 27;
+          $(this).append(newBeginDay);
+        }
+      }
+    }
+  });
+  $('#datePickerEndDay').on('swipeup', function(event) {
+      if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
+        if(newEndDay < 31) {
+          newEndDay = newEndDay + 1;
+          $(this).append(newEndDay);
+        } else {
+          newEndDay = newEndDay - 30;
+          $(this).append(newEndDay);
+        }
+      } else
+      if(i == 3 || i == 5 || i == 8 || i == 10) {
+        if(newEndDay < 30) {
+          newEndDay = newEndDay + 1;
+          $(this).append(newEndDay);
+        } else {
+          newEndDay = newEndDay - 29;
+          $(this).append(newEndDay);
+        }
+      } else
+      if(i == 1) {
+        if(newEndDay < 28) {
+          newEndDay = newEndDay + 1;
+          $(this).append(newEndDay);
+        } else {
+          newEndDay = newEndDay - 27;
+          $(this).append(newEndDay);
+        }
+      }
+    }
+  });
+  $('#datePickerEndDay').on('swipedown', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
+        if(newEndDay > 1) {
+          newEndDay = newEndDay - 1;
+          $(this).append(newEndDay);
+        } else {
+          newEndDay = newEndDay + 30;
+          $(this).append(newEndDay);
+        }
+      } else
+      if(i == 3 || i == 5 || i == 8 || i == 10) {
+        if(newEndDay > 1) {
+          newEndDay = newEndDay - 1;
+          $(this).append(newEndDay);
+        } else {
+          newEndDay = newEndDay + 29;
+          $(this).append(newEndDay);
+        }
+      } else
+      if(i == 1) {
+        if(newEndDay > 1) {
+          newEndDay = newEndDay - 1;
+          $(this).append(newEndDay);
+        } else {
+          newEndDay = newEndDay + 27;
+          $(this).append(newEndDay);
+        }
+      }
+    }
+  });
+
+  // month
+  $('#datePickerBeginMonth').on('swipeup', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i < 11) {
+        newBeginMonth = month[i = i + 1];
+        $(this).append(newBeginMonth);
+      } else {
+        newBeginMonth = month[i = i - 11];
+        $(this).append(newBeginMonth);
+      }
+
+      if(i == 3 || i == 5 || i == 8 || i == 10 && newBeginDay < 30) {
+        newBeginDay = 30;
+        $('#datePickerBeginDay').empty();
+        $('#datePickerBeginDay').append(newBeginDay);
+      }
+      if(i == 1 || i == 12 && newBeginDay < 28) { // 12 gibts nicht, aber ohne funktionierts nicht
+        newBeginDay = 28;
+        $('#datePickerBeginDay').empty();
+        $('#datePickerBeginDay').append(newBeginDay);
+      }
+    }
+  });
+  $('#datePickerBeginMonth').on('swipedown', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i > 0) {
+        newBeginMonth = month[i = i - 1];
+        $(this).append(newBeginMonth);
+      } else {
+        newBeginMonth = month[i = i + 11];
+        $(this).append(newBeginMonth);
+      }
+
+      if(i == 3 || i == 5 || i == 8 || i == 10 && newBeginDay < 30) {
+        newBeginDay = 30;
+        $('#datePickerBeginDay').empty();
+        $('#datePickerBeginDay').append(newBeginDay);
+      }
+      if(i == 1 || i == 12 && newBeginDay < 28) { // 12 gibts nicht, aber ohne funktionierts nicht
+        newBeginDay = 28;
+        $('#datePickerBeginDay').empty();
+        $('#datePickerBeginDay').append(newBeginDay);
+      }
+    }
+  });
+  $('#datePickerEndMonth').on('swipeup', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i < 11) {
+        newEndMonth = month[i = i + 1];
+        $(this).append(newEndMonth);
+      } else {
+        newEndMonth = month[i = i - 11];
+        $(this).append(newEndMonth);
+      }
+
+      if(i == 3 || i == 5 || i == 8 || i == 10 && newEndDay < 30) {
+        newEndDay = 30;
+        $('#datePickerEndDay').empty();
+        $('#datePickerEndDay').append(newEndDay);
+      }
+      if(i == 1 || i == 12 && newEndDay < 28) { // 12 gibts nicht, aber ohne funktionierts nicht
+        newEndDay = 28;
+        $('#datePickerEndDay').empty();
+        $('#datePickerEndDay').append(newEndDay);
+      }
+    }
+  });
+  $('#datePickerEndMonth').on('swipedown', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      if(i > 0) {
+        newEndMonth = month[i = i - 1];
+        $(this).append(newEndMonth);
+      } else {
+        newEndMonth = month[i = i + 11];
+        $(this).append(newEndMonth);
+      }
+
+      if(i == 3 || i == 5 || i == 8 || i == 10 && newEndDay < 30) {
+        newEndDay = 30;
+        $('#datePickerEndDay').empty();
+        $('#datePickerEndDay').append(newEndDay);
+      }
+      if(i == 1 || i == 12 && newEndDay < 28) { // 12 gibts nicht, aber ohne funktionierts nicht
+        newEndDay = 28;
+        $('#datePickerEndDay').empty();
+        $('#datePickerEndDay').append(newEndDay);
+      }
+    }
+  });
+
+  // year
+  $('#datePickerBeginYear').on('swipeup', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      newBeginYear = newBeginYear + 1;
+      $(this).append(newBeginYear);
+    }
+  });
+  $('#datePickerBeginYear').on('swipedown', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      if(newBeginYear > DEFAULTS.curryear) {
+        $(this).empty();
+        newBeginYear = newBeginYear - 1;
+        $(this).append(newBeginYear);
+      }
+    }
+  });
+  $('#datePickerEndYear').on('swipeup', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      $(this).empty();
+      newEndYear = newEndYear + 1;
+      $(this).append(newEndYear);
+    }
+  });
+  $('#datePickerEndYear').on('swipedown', function(event) {
+    if($('#editButtonAutofeed').hasClass('activated')) {
+      if(newEndYear > DEFAULTS.curryear) {
+        $(this).empty();
+        newEndYear = newEndYear - 1;
+        $(this).append(newEndYear);
+      }
+    }
+  });
+
+  // autofeed checkbox unlimited
+  $('#checkboxUnlimited').click(function() {
+    if($(this).hasClass('activated')) {
+      $(this).removeClass('activated');
+      $('.buttonsAutofeed').show();
+      $('.autofeedDate').show();
+      $('.autofeedOption').text('');
+    } else {
+      $(this).addClass('activated');
+      $('.buttonsAutofeed').hide();
+      $('.autofeedDate').hide();
+      if($('#autofeedToggle').hasClass('on')) {
+        $('.autofeedOption').text('unlimited');
+      } else
+      if($('#autofeedToggle').hasClass('off')) {
+        $('.autofeedOption').text('');
+      }
+    }
+  });
+
+
+
+
 
 
 
